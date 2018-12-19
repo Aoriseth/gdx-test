a0bc74b7d62f7ca0019a8cef84203195edfd0cd9
Lennart Cockx <lennart.cockx@gmail.com>
2018-12-19 12:46:33 +0100

    added Physics maps for sprites Modified world to use scaled down coordinates to make working with Box2D easier

--- core/src/me/cockx/learn/GdxGame.java
+++ core/src/me/cockx/learn/GdxGame.java
@@ -8,24 +8,40 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.util.HashMap;
 
 public class GdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture img;
 	private TextureAtlas textureAtlas;
-	private Sprite crate;
 	private ExtendViewport extendViewport;
 	private OrthographicCamera camera;
+	private final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>();
+	private World world;
+
+	private static final float STEP_TIME = 1f / 60f;
+	private static final int VELOCITY_ITERATIONS = 6;
+	private static final int POSITION_ITERATIONS = 2;
+	private static final float SCALE = 0.05f;
+
+	private float accumulator = 0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		textureAtlas = new TextureAtlas("sprites.txt");
-		crate = textureAtlas.createSprite("crate");
 		camera = new OrthographicCamera();
-		extendViewport = new ExtendViewport(800,600,camera);
+		extendViewport = new ExtendViewport(50,50,camera);
+		addSprites();
+		Box2D.init();
+		world = new World(new Vector2(0, -10), true);
 	}
 
 	@Override
@@ -33,26 +49,58 @@ public class GdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.57f, 0.77f, 0.85f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-//		batch.draw(img, 0, 0);
-//		batch.draw(img,250,250);
-		crate.setPosition(0,0);
-		crate.draw(batch);
-		crate.setPosition(crate.getWidth(),0);
-		crate.draw(batch);
-
+		drawSprite("crate",0,0);
+		drawSprite("crate",8,12);
+		drawSprite("cherries",30,0);
 		batch.end();
+		stepWorld();
 	}
-	
+
+	private void stepWorld() {
+		float delta = Gdx.graphics.getDeltaTime();
+
+		accumulator += Math.min(delta, 0.25f);
+
+		if (accumulator >= STEP_TIME) {
+			accumulator -= STEP_TIME;
+
+			world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		}
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
 		textureAtlas.dispose();
+		sprites.clear();
+		world.dispose();
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		extendViewport.update(width,height,true);
 		batch.setProjectionMatrix(camera.combined);
+	}
+
+	private void drawSprite(String name, float x, float y) {
+		Sprite sprite = sprites.get(name);
+		sprite.setPosition(x, y);
+		sprite.draw(batch);
+	}
+
+	private void addSprites() {
+		Array<TextureAtlas.AtlasRegion> regions = textureAtlas.getRegions();
+		for (TextureAtlas.AtlasRegion region : regions) {
+			Sprite sprite = textureAtlas.createSprite(region.name);
+			float width = sprite.getWidth()*SCALE;
+			float height = sprite.getHeight()*SCALE;
+
+			sprite.setSize(width,height);
+			sprite.setOrigin(0,0);
+
+			sprites.put(region.name, sprite);
+
+		}
 	}
 }
