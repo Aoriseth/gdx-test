9c81b0b57975530670e1fe34581afd1853a7c272
Lennart Cockx <lennart.cockx@gmail.com>
2018-12-20 11:56:20 +0100

    Made one of the crates controllable with WASD

--- core/src/me/cockx/learn/GdxGame.java
+++ core/src/me/cockx/learn/GdxGame.java
@@ -2,11 +2,13 @@ package me.cockx.learn;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Array;
@@ -84,6 +86,20 @@ public class GdxGame extends ApplicationAdapter {
 		drawSprite("crate",position2.x,position2.y,degrees2 );
 
 		renderFruitSprites();
+
+		if (Gdx.input.isKeyPressed(Input.Keys.F)){
+			crate.applyLinearImpulse(0,50,crate.getPosition().x,crate.getPosition().y,false);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.T)){
+			crate.applyLinearImpulse(50,0,crate.getPosition().x,crate.getPosition().y,false);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.R)){
+			crate.applyLinearImpulse(-50,0,crate.getPosition().x,crate.getPosition().y,false);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.S)){
+			crate.applyLinearImpulse(0,-50,crate.getPosition().x,crate.getPosition().y,false);
+		}
+
 
 		batch.end();
 		stepWorld();
